5b28501b773548f5198b30ca7a8cb87fa52ebe75
sebastian <sebastian@simplaex.com>
2020-10-05 20:33:23 +0200

    fix image conversion

--- src/main/scala/com/github/arzt/tensor/image/ImageTool.scala
+++ src/main/scala/com/github/arzt/tensor/image/ImageTool.scala
@@ -4,7 +4,10 @@ package image
 import java.awt.image.BufferedImage
 import java.awt.image.DataBuffer
 import java.awt.image.DataBufferByte
+import java.awt.image.DataBufferInt
 import java.lang.Double.longBitsToDouble
+
+import com.github.arzt.tensor.convert.implicits.intToByte
 
 object ImageTool {
 
@@ -13,6 +16,9 @@ object ImageTool {
       case bb: DataBufferByte =>
         val data = bb.getBankData()(0)
         new ArrayTensor[Byte](Vector(bb.getSize), data)
+      case bb: DataBufferInt =>
+        val data = bb.getBankData()(0)
+        new ArrayTensor[Int](Vector(bb.getSize), data).inflate[Byte]
       case _ =>
         import convert.implicits._
         val data = (0 until buffer.getSize).map(buffer.getElemDouble).toArray
@@ -27,27 +33,28 @@ object ImageTool {
       case bb: DataBufferByte =>
         val data = bb.getBankData()(0)
         tensor.cached(data)
-        buffer
+      case bb: DataBufferInt =>
+        val data = bb.getBankData()(0)
+        tensor.deflate[Int].cached(data)
       case _ =>
         import convert.implicits._
-        val values = tensor.deflate[Long].map(longBitsToDouble)
+        val values = tensor.reshape(tensor.length).deflate[Long].map(longBitsToDouble)
         var i = 0
         val valueIterator = values.toIterable.iterator
         while (i < buffer.getSize) {
           buffer.setElemDouble(i, valueIterator.next())
           i += 1
         }
-        buffer
     }
+    buffer
   }
 
   def fromImage(image: BufferedImage): Tensor[Byte] = {
     val buffer = image.getRaster.getDataBuffer
-    dataBufferToBytes(buffer)
-      .reshape(
-        image.getHeight,
-        image.getWidth,
-        image.getColorModel.getNumComponents)
+    val height = image.getHeight
+    val width = image.getWidth
+    val components = image.getColorModel.getNumComponents
+    dataBufferToBytes(buffer).reshape(height, width, components)
   }
 
   def writeImage(image: BufferedImage, tensor: Tensor[Byte]): BufferedImage = {
--- src/test/scala/com/github/arzt/tensor/image/ImageToolSpec.scala
+++ src/test/scala/com/github/arzt/tensor/image/ImageToolSpec.scala
@@ -1,8 +1,8 @@
 package com.github.arzt.tensor
 package image
 
-import java.awt.image.BufferedImage
-import java.awt.image.DataBufferByte
+import java.awt.image.BufferedImage.TYPE_3BYTE_BGR
+import java.awt.image.BufferedImage.TYPE_INT_ARGB
 
 import com.github.arzt.tensor.TensorImplicits._
 import org.specs2.mutable.Specification
@@ -13,27 +13,21 @@ class ImageToolSpec extends Specification {
   "ImageTool" should {
     val width = 4
     val height = 3
-    val bytes = new Array[Byte](width * height * 3)
+    val bytes = new Array[Byte](width * height * 4)
     nextBytes(bytes)
 
-    "convert image to tensor" in {
-      val image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR)
-      val data = image.getRaster.getDataBuffer.asInstanceOf[DataBufferByte].getBankData()(0)
-      Array.copy(bytes, 0, data, 0, bytes.length)
-      val tensor = image.asTensor
-      tensor.shape === Seq(height, width, 3)
-      tensor.sameElements(bytes)
-    }
-    "covert tensor to image" in {
-      val image = Tensor(data = bytes, height, width, 3).asImage(BufferedImage.TYPE_3BYTE_BGR)
+    "convert tensor to image (3 byte type) and back" in {
+      val value1 = bytes.asTensor(height, width, 4)
+      val source = value1(::, ::, 1 :: -1)
+      val image = source.asImage(TYPE_3BYTE_BGR)
+      val target = image.asTensor
       image.getWidth === width
       image.getHeight === height
-      image
-        .getData
-        .getDataBuffer
-        .asInstanceOf[DataBufferByte]
-        .getBankData()(0)
-        .sameElements(bytes)
+      source.sameElements(target) must beTrue
+    }
+    "convert tensor to image (4 byte type) and back" in {
+      val source = bytes.asTensor(height, width, 4)
+      source.asImage(TYPE_INT_ARGB).asTensor.sameElements(source) must beTrue
     }
   }
 }
